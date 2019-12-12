b7a4ac6dac4badb3bbf7b7df6051aa3d30d85ecc
jasonxia17 <jasonx3@illinois.edu>
2019-12-12 17:46:23 -0600

    Wrote constructor for a new Go game

--- src/go_engine/core.clj
+++ src/go_engine/core.clj
@@ -1,6 +1,13 @@
 (ns go-engine.core
   (:gen-class))
 
+(defrecord GoGame [matrix size turn])
+
+(defn create-new-game
+  "Creates a size x size GoGame where board is blank and it's black's turn"
+  [size]
+  (GoGame. (vec (repeat size (vec (repeat size :empty)))) size :black))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/go_engine/core_test.clj
+++ test/go_engine/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [go-engine.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest create-new-game-test
+  (let [game (create-new-game 2)]
+    (testing "Board matrix is correct"
+      (is (= (:matrix game)
+             [[:empty :empty]
+              [:empty :empty]])))
+    (testing "Board size is correct"
+      (is (= (:size game) 2)))
+    (testing "Next player turn is correct"
+      (is (= (:turn game) :black)))))
